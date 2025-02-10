e898dbb9dc145ec965d2664bb0d9845465bc6bd1
Maitry-101 <shindemaitry@gmail.com>
2025-02-10 09:52:02 +0530

    first commit

---
+++ Website/app2.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+# ---- HEADER SECTION ----
+
+st.subheader("Hi, I am Sven :wave:")
+
+st.title("A Data Analyst From Germany")
+
+st.write("I am passionate about finding ways to use Python and VBA to be more efficient.")
+st.write("[Learn More >](https://pythonandvba.com)")
